f1f624a8f72e0a7a1f9eb300d7bd3b95ea92bb21
Diego Brandjes <diego.brandjes@hva.nl>
2025-01-19 02:37:48 +0100

    style changes to add parallax

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,8 +4,7 @@ import './navbar.css';
 
 const VerticalMenu: React.FC = () => {
   return (
-    <div className="poppins-bold vertical-menu">
-      <div className='margins'>
+    <div className="vertical-menu">
       <a href="/home" aria-current="page" className="">
             <img 
               src="/logo.svg"
@@ -15,14 +14,13 @@ const VerticalMenu: React.FC = () => {
             />
           </a>
       
-      <nav>
+      <nav className='poppins-bold'>
         <ul>
           <li><a href="#home">Home</a></li>
           <li><a href="#blog">Blog</a></li>
           <li><a href="#about">About</a></li>
         </ul>
       </nav>
-      </div>
     </div>
   );
 };
--- src/homepage/home.tsx
+++ src/homepage/home.tsx
@@ -3,21 +3,48 @@ import VerticalMenu from "../components/navbar.tsx"
 function home() {
     return (
         <body className="body">
-            <VerticalMenu />
+        <VerticalMenu />
+        
+        <main>       
 
-            <div className="banner">
-                <div className="banner-text">
-                    <div className="together">
-                        <h1 className="poppins-bold">Japan</h1>
-                        <h2 className="reddit-mono">'23</h2>
-                    </div>
-                        <p className="poppins-regular">A seven Month journey through the land of the rising sun.</p>
+        <section className="parallax bg"></section>
+
+        <section className="page">
+            {/* <p>The sound that occurs when you snap your fingers is made by your middle finger hitting your palm!</p> */}
+            <div className="image-gallery">
+ 
+                <div className="image-card">
+                    <img src="tokyo-bay.jpg" alt="Osaka Castle" />
+                    {/* <div className="caption">
+                    <p>大阪</p>
+                    </div> */}
                 </div>
             </div>
 
+            <div className="image-gallery">
+                <div className="image-card">
+                    <img src="matsumoto.jpg" alt="Osaka Castle" />
+                    {/* <div className="caption">
+                    <p>大阪</p>
+                    </div> */}
+                </div>
+            </div>
 
-            
+            <div className="image-gallery">
+                <div className="image-card">
+                    <img src="okinawa.jpg" alt="Osaka Castle" />
+                    {/* <div className="caption">
+                    <p>大阪</p>
+                    </div> */}
+                </div>
+            </div>
+        </section>
+        <section className="parallax bg"></section>
 
+
+        
+
+        </main>
         </body>
     );
 }
