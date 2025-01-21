bb94917caad0746b8e0ecfe0f2eea9e65b6954d2
Diego Brandjes <diego.brandjes@hva.nl>
2025-01-21 03:45:52 +0100

    created about page grid

--- src/about/about.tsx
+++ src/about/about.tsx
@@ -1,7 +1,38 @@
+import VerticalMenu from "../components/navbar.tsx"
+
+
 function about() {
     return (
         // Code here
-        <h1>About Here</h1>
-    );
+        <body className="body">
+        
+        <main>    
+
+        <div className="container">
+            <div className="bento-box poppins-regular">
+                <div className="item small">    
+                    <nav className='poppins-bold'>
+                        <ul>
+                        <li><a href="/home">Home</a></li>
+                        <li><a href="/blog">Blog</a></li>
+                        </ul>
+                    </nav>
+                </div>
+                <div className="item large">Large Item</div>
+                <div className="item medium">About Page</div>
+                <div className="item small">Small Item</div>
+                <div className="item small">Small Item</div>
+                <div className="item large">Large Item</div>
+                <div className="item long">Long Item</div>
+
+                <div className="item small">Small Item</div>
+                <div className="item small">Small Item</div>
+                <div className="item small">Small Item</div>
+            </div>
+        </div>
+        </main>
+        </body>
+
+            );
 }
 export default about;
