1fae164298ed30702aef0db68151eb526ec629f4
Diego Brandjes <diego.brandjes@hva.nl>
2025-01-23 22:04:31 +0100

    fixed sizing of logo

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -5,12 +5,12 @@ import './navbar.css';
 const VerticalMenu: React.FC = () => {
   return (
     <div className="vertical-menu">
-      <a href="/home" aria-current="page" className="vertical-menu-item">
+      <a href="/home" aria-current="page" className="">
             <img 
               src="/logo.svg"
               loading="lazy" 
               alt="Logo" 
-              className="logo" 
+              className="vertical-menu-item" 
             />
           </a>
       
