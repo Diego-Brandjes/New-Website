416315e0495705b9dee61457a8716b74f42a393b
Diego Brandjes <diego.brandjes@hva.nl>
2025-01-23 21:52:54 +0100

    added yeetable 404 symbol

---
+++ src/components/throw.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import * as motion from "motion/react-client";
+
+export default function Drag() {
+    return (
+        <motion.div drag style={box}>
+            <p style= {textStyle}>404</p>
+        </motion.div>
+    );
+}
+
+/**
+ * ==============   Styles   ================
+ */
+
+const box: React.CSSProperties = {
+    width: 150,
+    height: 150,
+    backgroundColor: "white",
+    borderRadius: 10,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    cursor: "grab", // Show grab cursor for draggable items
+};
+
+const textStyle: React.CSSProperties = {
+    fontFamily: "poppins",
+    fontSize: "4vh",
+    fontWeight: "bold",
+    color: "black",
+    textAlign: "center",
+};
--- src/notfound/notfound.tsx
+++ src/notfound/notfound.tsx
@@ -1,13 +1,19 @@
+import Drag from "../components/throw";
+
 function NotFound() {
     return (
         <div className="notfound-page">
+            <div className="drag-container">
+                <Drag />
+            </div>
             <div className="notfound-content">
-                <h1 className=" poppins-bold">Oops!</h1>
-                <h2 className=" poppins-thin">The page you are looking for doesn't exist or has been moved.</h2>
-                    
+                <h2 className="poppins-thin">
+                    The page you are looking for doesn't exist or has been moved.
+                </h2>
             </div>
         </div>
     );
 }
+
 
 export default NotFound;
